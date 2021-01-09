2378ed95eb8e750dc1b9911ba23694c92f9d6e37
Gladozzz <beerulandroid@outlook.com>
2021-01-09 13:44:52 +0500

    bueti

--- src/components/threejsview.jsx
+++ src/components/threejsview.jsx
@@ -28,9 +28,9 @@ class Threejsview extends Component {
         super(props);
 
         this.handleDrawOnClick = this.handleDrawOnClick.bind(this);
-        this.handleWidthChange = this.handleWidthChange.bind(this);
-        this.handleHeightChange = this.handleHeightChange.bind(this);
-        this.handleLengthChange = this.handleLengthChange.bind(this);
+        // this.handleWidthChange = this.handleWidthChange.bind(this);
+        // this.handleHeightChange = this.handleHeightChange.bind(this);
+        // this.handleLengthChange = this.handleLengthChange.bind(this);
         // this.renderThreejs = this.renderThreejs.bind(this);
 
         this.state = {
@@ -43,41 +43,59 @@ class Threejsview extends Component {
         };
     }
 
-    handleWidthChange(event) {
-        if (!isNaN(event.target.value) && event.target.value < 1000) {
-            this.setState({ widthValid: true });
-            this.widthInput.className = "goodInput";
-        } else {
-            this.widthInput.className = "badInput";
-            this.setState({ widthValid: false });
-        }
-    }
+    // handleWidthChange(event) {
+    //     if (!isNaN(event.target.value)) {
+    //         this.setState({ widthValid: true });
+    //         this.widthInput.className = "goodInput";
+    //     } else {
+    //         this.widthInput.className = "badInput";
+    //         this.setState({ widthValid: false });
+    //     }
+    // }
 
-    handleHeightChange(event) {
-        if (!isNaN(event.target.value) && event.target.value < 1000) {
-            this.setState({ heigthValid: true });
-            this.heigthInput.className = "goodInput";
-        } else {
-            this.heigthInput.className = "badInput";
-            this.setState({ heigthValid: false });
-        }
-    }
+    // handleHeightChange(event) {
+    //     if (!isNaN(event.target.value)) {
+    //         this.setState({ heigthValid: true });
+    //         this.heigthInput.className = "goodInput";
+    //     } else {
+    //         this.heigthInput.className = "badInput";
+    //         this.setState({ heigthValid: false });
+    //     }
+    // }
 
-    handleLengthChange(event) {
-        if (!isNaN(event.target.value) && event.target.value < 1000) {
-            this.setState({ lengthValid: true });
-            this.lengthInput.className = "goodInput";
-        } else {
-            this.lengthInput.className = "badInput";
-            this.setState({ lengthValid: false });
-        }
-    }
+    // handleLengthChange(event) {
+    //     if (!isNaN(event.target.value)) {
+    //         this.setState({ lengthValid: true });
+    //         this.lengthInput.className = "goodInput";
+    //     } else {
+    //         this.lengthInput.className = "badInput";
+    //         this.setState({ lengthValid: false });
+    //     }
+    // }
 
     handleDrawOnClick(event) {
         if (this.state.widthValid && this.state.heigthValid && this.state.lengthValid) {
             var w = this.widthInput.value
             var h = this.heigthInput.value
             var l = this.lengthInput.value
+
+            var dif = 1;
+            if (w > h && w > l && w > 1000) {
+                dif = w / 1000; 
+                console.log("dif w");
+            } else if (h > w && h > l && h > 1000) {
+                dif = h / 1000; 
+                console.log("dif h");
+            } else if (l > 1000) {
+                dif = l / 1000;
+                console.log("dif l");
+            }
+            console.log("dif " + dif);
+            if (dif > 1) {
+                w = (w / dif).toFixed(5);
+                h = (h / dif).toFixed(5);
+                l = (l / dif).toFixed(5);
+            }
             fetch('/calculate?width=' + w + '&height=' + h + '&length=' + l)
                 .then(response => response.json())
                 .then(json => {
@@ -115,13 +133,13 @@ class Threejsview extends Component {
             <div className='threejsview'>
                 <div className='inputs'>
                     <div className='boxSizeInput'>
-                        <label htmlFor="winput">Enter width (max: 1000):</label><input onChange={this.handleWidthChange} className="goodInput" id='winput' defaultValue={defaultProps.width} ref={c=>this.widthInput=c}/>
+                        <label htmlFor="winput">Enter width:</label><input className="goodInput" id='winput' defaultValue={defaultProps.width} type="number" min={0} ref={c=>this.widthInput=c}/>
                     </div>
                     <div className='boxSizeInput'>
-                        <label htmlFor="hinput">Enter height (max: 1000):</label><input onChange={this.handleHeightChange} className="goodInput" id='hinput' defaultValue={defaultProps.height} ref={c=>this.heigthInput=c}/>
+                        <label htmlFor="hinput">Enter height:</label><input className="goodInput" id='hinput' defaultValue={defaultProps.height} type="number" min={0} ref={c=>this.heigthInput=c}/>
                     </div>
                     <div className='boxSizeInput'>
-                        <label htmlFor="linput">Enter length (max: 1000):</label><input onChange={this.handleLengthChange} className="goodInput" id='linput' defaultValue={defaultProps.length} ref={c=>this.lengthInput=c}/>
+                        <label htmlFor="linput">Enter length:</label><input className="goodInput" id='linput' defaultValue={defaultProps.length} type="number" min={0} ref={c=>this.lengthInput=c}/>
                     </div>
                     <Button
                         id="drawButton"
